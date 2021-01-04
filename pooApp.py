bb874ea6ee51f1f83e6f07330f2971cecce75183
karacop <76942786+karacop@users.noreply.github.com>
2021-01-04 10:49:14 +0100

    Add files via upload

---
+++ pooApp.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+
+pygame.init()
+
+
+class Game:
+    def __init__(self):
+        self.setup_display()
+        self.alien()
+        self.enemy()
+
+    def setup_display(self):
+        self.screen_size = [360, 600]
+        self.screen = pygame.display.set_mode(self.screen_size)
+        self.screen = pygame.display.set_caption("Dodge Game")
+        self.screen = pygame.image.load("bg.png")
+
+    def alien(self):
+        self.alien_1 = pygame.image.load("alien.png")
+        self.alien_1 = pygame.transform.scale(self.alien_1, (105, 105))
+    
+    def enemy(self):
+        self.enemy_1 = pygame.image.load("enemy.png")
+        self.enemy_1 = pygame.transform.scale(self.enemy_1, (120, 120))
+
+
+game = Game()
+
+
+running = True
+while running:
+    
+    game.setup_display()
+    game.alien()
+    game.enemy()
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("game over")
+            
